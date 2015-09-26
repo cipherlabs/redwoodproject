7fcaf70e232dfabbd4c8832b827d77afd9e9ccd6
David Gee <davidlguida@gmail.com>
2015-09-26 15:50:12 -0400

    request a demo added

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,4 @@
-/* ================================
+  /* ================================
 ===  BACKGROUND SLIDER        ====
 ================================= */
 $.vegas('slideshow', {
@@ -7,6 +7,7 @@ $.vegas('slideshow', {
     { src:'images/backgrounds/bg1.jpg', fade:1000 },
     { src:'images/backgrounds/bg2.jpg', fade:1000 },
     { src:'images/backgrounds/bg3.jpg', fade:1000 }
+    { src:'images/backgrounds/bg4.jpg', fade:1000 }    
   ]
 });
 var mainbottom = $('#main-nav').offset().top + $('#main-nav').height();
